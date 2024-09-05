0e3690d7fea7e6706cc7038423a72d203c0e0ab2
Matthew Logan <loganmatthewdev@gmail.com>
2024-09-04 21:31:03 -0700

    Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <></>;
+  const [motionEvent, setMotionEvent] = useState<DeviceMotionEvent>();
+  const handleMotionChange = (evt: DeviceMotionEvent) => setMotionEvent(evt);
+
+  useEffect(() => {
+    window.addEventListener("devicemotion", handleMotionChange);
+  });
+
+  return (
+    <div>
+      <h1>device-motion-event-example</h1>
+      <h2>Content</h2>
+      <nav>
+        <h2>Links</h2>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/LocalNewsTV/device-motion-event-example"
+              rel="author"
+              target="_blank"
+            >
+              Repo
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/LocalNewsTV"
+              rel="author"
+              target="_blank"
+            >
+              Creator
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Acceleration</h2>
+        <ul>
+          <li>X: {motionEvent?.acceleration?.x ?? 0}</li>
+          <li>Y: {motionEvent?.acceleration?.y ?? 0}</li>
+          <li>Z: {motionEvent?.acceleration?.z ?? 0}</li>
+        </ul>
+        <h2>Acceleration Including Gravity</h2>
+        <ul>
+          <li>X: {motionEvent?.accelerationIncludingGravity?.x ?? 0}</li>
+          <li>Y: {motionEvent?.accelerationIncludingGravity?.y ?? 0}</li>
+          <li>Z: {motionEvent?.accelerationIncludingGravity?.z ?? 0}</li>
+        </ul>
+      </main>
+    </div>
+  );
 };
 
 export default App;
